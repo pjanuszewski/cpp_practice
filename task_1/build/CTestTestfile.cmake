3b1f7fbe5745121fa2b9b38aac1d0070b3a4d5f1
Pawel Januszewski <akurat444@gmail.com>
2024-04-01 23:06:25 +0200

    Task_1

---
+++ task_1/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pawelj/Git_repos/cpp_practice/task_1
+# Build directory: /home/pawelj/Git_repos/cpp_practice/task_1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
